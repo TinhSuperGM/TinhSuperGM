e032050ce74c1048772e7d1a7a1ec36d7c2ba637
TinhSuperGM <tinhsuper1606@gmail.com>
2026-07-17 20:48:55 +0700

    Update and rename main2.lua to main3.lua

--- main2.lua
+++
@@ -1,9 +0,0 @@
--- ────────────────────────────────────────────────
---  WRACOX AURA - BYPASS KEY SYSTEM PERFECTLY
--- ────────────────────────────────────────────────
-
--- Bước 1: Khai báo bỏ qua hệ thống Key của Wraithh
-local KeySystem = false 
-
--- Bước 2: Tải và thực thi file mã hóa tính năng gốc từ GitHub của tác giả
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Wraithi/Wraithcox/refs/heads/main/realwraithcoxcox.txt"))()
